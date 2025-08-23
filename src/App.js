058252e21b2a69b1eaea888f472b68f5b33238dc
Baswal Ganesh <67188332+baswalganesh@users.noreply.github.com>
2025-08-23 15:13:19 +0530

    added local storage for noreset refresh

--- src/App.js
+++ src/App.js
@@ -3,32 +3,17 @@ import './App.css'
 import Header from './components/Header'
 import AddTask from './components/AddTask'
 import ShowTask from './components/ShowTask'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Counter from './components/Counter'
 
 
 
 export default function App() {
-  const [tasks,setTasks]=useState([
-    {
-      "id": 1,
-      "name": "finish coding assignment",
-      "time": "2025-08-20T11:00:00Z"
-    },
-    {
-      "id": 2,
-      "name": "buy groceries for the week",
-      "time": "2025-08-21T15:30:00Z"
-    },
-    {
-      "id": 3,
-      "name": "attend gym session",
-      "time": "2025-08-22T09:00:00Z"
-    } 
-  ]
-  
-  )
-  
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
 
   return (
     <>
